821e1eea730d26a23b77cf208326543640e2cba5
julien <julienchabanne@rodin4d.com>
2024-01-07 17:47:17 +0100

    clean code example

---
+++ computeV1.js
@@ -0,0 +1,10 @@
+function compute(arr) {
+    const result = [];
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] % 2 === 0) {
+            result.push(arr[i]);
+        }
+    }
+    return result;
+}
+
---
+++ computeV3.js
@@ -0,0 +1,8 @@
+function getEvenNumbers(numbers) {
+    return numbers.filter(isEvenNumber);
+}
+
+function isEvenNumber(number) {
+    return number % 2 === 0;
+}
+
